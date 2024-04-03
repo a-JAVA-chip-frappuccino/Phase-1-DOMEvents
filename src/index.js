04b13a72514da9b04ab4833913fbe3e491ac941a
Eleanor Kelman <ekelman3@gmail.com>
2024-04-03 12:54:36 -0400

    prepped lecture

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/* ----------------------------------------------------- 
+    VOCABULARY TERMS
+----------------------------------------------------- */
+
+/*
+
+    - event listener: an action performed by the computer triggered by the user
+        - event: an action performed by the user that triggers a computer action (such as a mouseclick)
+    - function invocation: allowing a function to be run (rather than running it automatically)
+
+*/
+
+/* ----------------------------------------------------- 
+    HOVERING EVENT LISTENERS
+----------------------------------------------------- */
+
+
+
+/* ----------------------------------------------------- 
+    CLICKING EVENT LISTENERS
+----------------------------------------------------- */
+
+
+
+/* ----------------------------------------------------- 
+    SUBMIT EVENT LISTENERS
+----------------------------------------------------- */
+
